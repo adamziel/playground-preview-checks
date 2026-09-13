ce0ddb9b603d74c5b9b80dbe2b570d87842674db
Adam Zieliński <adam@adamziel.com>
2026-09-13 02:10:20 +0200

    Exercise recipe revision 2

--- plugins/site-toolkit/site-toolkit.php
+++ plugins/site-toolkit/site-toolkit.php
@@ -1,3 +1,5 @@
 <?php
 /* Plugin Name: Preview site-toolkit */
 add_action('admin_notices', function () { echo '<div class="notice notice-info"><p>Preview fixture: site-toolkit</p></div>'; });
+
+// Recipe revision 2
