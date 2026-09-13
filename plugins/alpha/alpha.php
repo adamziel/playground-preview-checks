29d6ad75d96b475216fb1b5c8f81ca9f2abbec18
Adam Zieliński <adam@adamziel.com>
2026-09-13 02:01:44 +0200

    Exercise build-selective recipe

--- plugins/alpha/alpha.php
+++ plugins/alpha/alpha.php
@@ -1,3 +1,5 @@
 <?php
 /* Plugin Name: Preview alpha */
 add_action('admin_notices', function () { echo '<div class="notice notice-info"><p>Preview fixture: alpha</p></div>'; });
+
+// Recipe check: baseline/build-selective
